639a1eeabaeaffdd4db51b63ae3640469af57c3b
YasserAtiya <ya13@my.fsu.edu>
2015-10-17 09:48:18 -0400

    10/17/2015
    Add Internship.ApplicationLink tuple. Structural changes.

--- DatabaseDump.sql
+++ DatabaseDump.sql
@@ -30,7 +30,8 @@ CREATE TABLE `Internship` (
   `Span` varchar(45) NOT NULL,
   `Requirements` varchar(45) DEFAULT NULL,
   `Applicants` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`Position`),
+  `ApplicationLink` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Position`,`Company`),
   KEY `Provider` (`Company`),
   CONSTRAINT `Provider` FOREIGN KEY (`Company`) REFERENCES `Company` (`Name`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -42,7 +43,7 @@ CREATE TABLE `Internship` (
 
 LOCK TABLES `Internship` WRITE;
 /*!40000 ALTER TABLE `Internship` DISABLE KEYS */;
-INSERT INTO `Internship` VALUES ('Development','State Farm','Summer 2016','Graduate','');
+INSERT INTO `Internship` VALUES ('Development','State Farm','Summer 2016','Graduate','',NULL);
 /*!40000 ALTER TABLE `Internship` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,11 +83,11 @@ DROP TABLE IF EXISTS `Student`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Student` (
   `StudentID` varchar(40) NOT NULL,
-  `Name` varchar(45) NOT NULL,
+  `FName` varchar(45) NOT NULL,
+  `LName` varchar(45) NOT NULL,
   `GPA` double DEFAULT NULL,
   `Interest` varchar(45) DEFAULT NULL,
-  `Email` varchar(45) DEFAULT NULL,
-  `Password` varchar(100) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`StudentID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -97,7 +98,7 @@ CREATE TABLE `Student` (
 
 LOCK TABLES `Student` WRITE;
 /*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES ('aav13','Arturo Valey',4,NULL,NULL,NULL),('cdp13c','Christian Patino',3.95,'Databases',NULL,NULL),('pn12','Poonyaporn Nanthakijjar',3.7,'Web',NULL,NULL),('ya13','Yasser Atiya',3.2,'Development',NULL,NULL);
+INSERT INTO `Student` VALUES ('aav13','Arturo','Valley',4,NULL,NULL),('cdp13c','Christian','Patino',3.95,'Databases',NULL),('pn12','Poonyaporn','Nanthakijjar',3.7,'Web',NULL),('ya13','Yasser','Atiya',3.2,'Development',NULL);
 /*!40000 ALTER TABLE `Student` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -136,4 +137,33 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-10-09 18:07:40
+-- Dump completed on 2015-10-17  9:40:56
+CREATE DATABASE  IF NOT EXISTS `mysql` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `mysql`;
+-- MySQL dump 10.13  Distrib 5.5.40, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: mysql
+-- ------------------------------------------------------
+-- Server version	5.5.40-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-10-17  9:40:56
