10ae6aa3a474828d9c7eb7aedd6aef98d9bb6d80
Christian <cdp13c@my.fsu.edu>
2015-11-07 14:40:58 -0500

    Updated Code for Database Changes

--- interncsfsu/company/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Database constants for the Company table
-"""
-MAX_NAME = 40
-MAX_CONTACT = 45
-MAX_WEBSITE = 100
-MAX_USERNAME = 45
-MAX_PASSWORD = 45
--- interncsfsu/student/constants.py
+++
@@ -1,8 +0,0 @@
-"""
-Student constants for the Student table
-"""
-MAX_SID = 40
-MAX_NAME = 45
-MAX_INTEREST = 45
-MAX_EMAIL = 45
-MAX_PASSWORD = 100
